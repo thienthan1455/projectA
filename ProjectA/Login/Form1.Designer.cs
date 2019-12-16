2b5a5659c880921b8af979fcf5cc8215ff62ced0
ThaiBinh <thienthan1455@gmail.com>
2019-12-16 13:54:37 +0700

    update

---
+++ ProjectA/Login/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbshowpassword = new System.Windows.Forms.CheckBox();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtaccount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblpassword = new System.Windows.Forms.Label();
+            this.lblaccount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbshowpassword
+            // 
+            this.cbshowpassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbshowpassword.AutoSize = true;
+            this.cbshowpassword.Location = new System.Drawing.Point(196, 213);
+            this.cbshowpassword.Margin = new System.Windows.Forms.Padding(4);
+            this.cbshowpassword.Name = "cbshowpassword";
+            this.cbshowpassword.Size = new System.Drawing.Size(129, 21);
+            this.cbshowpassword.TabIndex = 15;
+            this.cbshowpassword.Text = "Show Password";
+            this.cbshowpassword.UseVisualStyleBackColor = true;
+            // 
+            // btncancel
+            // 
+            this.btncancel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btncancel.Location = new System.Drawing.Point(301, 260);
+            this.btncancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(100, 28);
+            this.btncancel.TabIndex = 14;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnlogin.Location = new System.Drawing.Point(78, 260);
+            this.btnlogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(100, 28);
+            this.btnlogin.TabIndex = 13;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtpassword.Location = new System.Drawing.Point(157, 167);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(243, 22);
+            this.txtpassword.TabIndex = 12;
+            this.txtpassword.UseSystemPasswordChar = true;
+            // 
+            // txtaccount
+            // 
+            this.txtaccount.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtaccount.Location = new System.Drawing.Point(157, 97);
+            this.txtaccount.Margin = new System.Windows.Forms.Padding(4);
+            this.txtaccount.Name = "txtaccount";
+            this.txtaccount.Size = new System.Drawing.Size(243, 22);
+            this.txtaccount.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(201, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 39);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Login";
+            // 
+            // lblpassword
+            // 
+            this.lblpassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblpassword.AutoSize = true;
+            this.lblpassword.Location = new System.Drawing.Point(40, 171);
+            this.lblpassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblpassword.Name = "lblpassword";
+            this.lblpassword.Size = new System.Drawing.Size(69, 17);
+            this.lblpassword.TabIndex = 9;
+            this.lblpassword.Text = "Password";
+            // 
+            // lblaccount
+            // 
+            this.lblaccount.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblaccount.AutoSize = true;
+            this.lblaccount.Location = new System.Drawing.Point(40, 101);
+            this.lblaccount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblaccount.Name = "lblaccount";
+            this.lblaccount.Size = new System.Drawing.Size(59, 17);
+            this.lblaccount.TabIndex = 8;
+            this.lblaccount.Text = "Account";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 346);
+            this.Controls.Add(this.cbshowpassword);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtaccount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblpassword);
+            this.Controls.Add(this.lblaccount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbshowpassword;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtaccount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblpassword;
+        private System.Windows.Forms.Label lblaccount;
+    }
+}
+
---
+++ ProjectA/Login/Logiclayer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class Logiclayer
+    {
+        public List<Cauthu> Getplayers()
+        {
+            var db = new QLCTEntities();
+            List<Cauthu> players = db.Cauthu.ToList();
+
+            return players;
+        }
+        public Cauthu GetPlayer(int id)
+        {
+            var db = new QLCTEntities();
+            var player = db.Cauthu.Find(id);
+            return player;
+        }
+        public void AddPlayer(string TenCauThu, string Gioitinh, DateTime NgaySinh, string Nickname, string Quequan, string ChieuCao, string Vitri, string Soao, string clb)
+        {
+            var db = new QLCTEntities();
+            var player = new Cauthu();
+
+            player.TenCauThu = TenCauThu;
+            player.Gioitinh = Gioitinh;
+            player.Ngaysinh = NgaySinh;
+            player.Nickname = Nickname;
+            player.Quequan = Quequan;
+            player.Chieucao = ChieuCao;
+            player.Vitri = Vitri;
+            player.Soao = Soao;
+            player.clb = clb;
+
+            db.Cauthu.Add(player);
+            db.SaveChanges();
+        }
+        public Login[] Getlogin()
+        {
+            var db = new QLCTEntities();
+            return db.Login.ToArray();
+        }
+        public void DeletePlayer(int id)
+        {
+            var db = new QLCTEntities();
+            var player = db.Cauthu.Find(id);
+
+            db.Cauthu.Remove(player);
+            db.SaveChanges();
+        }
+        public void EditPlayer(int id, string TenCauThu, string Gioitinh, DateTime NgaySinh, string Nickname, string Quequan, string ChieuCao, string Vitri, string Soao, string clb)
+        {
+            var db = new QLCTEntities();
+            var currentPlayer = db.Cauthu.Find(id);
+
+            currentPlayer.TenCauThu = TenCauThu;
+            currentPlayer.Gioitinh = Gioitinh;
+            currentPlayer.Ngaysinh = NgaySinh;
+            currentPlayer.Nickname = Nickname;
+            currentPlayer.Quequan = Quequan;
+            currentPlayer.Chieucao = ChieuCao;
+            currentPlayer.Vitri = Vitri;
+            currentPlayer.Soao = Soao;
+            currentPlayer.clb = clb;
+
+            db.Entry(currentPlayer).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+        }
+    }
+}
+
